c39cf81eae12bbf835a6000209c240b2ec270916
RavinduKodithuwakku <ravinduransara9@gmail.com>
2023-06-16 23:06:47 +0530

    project build

---
+++ ecommerce-admin/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div  className="bg-blue-900 w-screen h-screen flex items-center">
+    <dir className="text-center w-full">
+    <button className="bg-white p-2 px-4 rounded-lg">Login with Google </button>
+    </dir>
+      
+    </div>
+  )
+}
